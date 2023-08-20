f84855411eba8b8baea4362e2405d227690112fe
Bruno Silva <fawnsteak@gmail.com>
2023-08-20 18:29:30 -0300

    Criação do método Cadastrar Filme

--- src/main/java/br/com/sistemadefilmes/service/FilmeService.java
+++ src/main/java/br/com/sistemadefilmes/service/FilmeService.java
@@ -1,12 +1,22 @@
 package br.com.sistemadefilmes.service;
-
+import java.util.ArrayList;
 // Implementar métodos e mudar se necessário o tipo de retorno.
 public class FilmeService {
+    private ArrayList<Filme> filmes;
+    public FilmeService() {
+        filmes = new ArrayList<Filme>();
 
-    public void cadastrarFilme(){}
+    public void cadastrarFilme(Filme filme){
+            if (filmes.contains(filme)) {
+                System.out.println("O filme já está cadastrado.");
+                return false;
+            }
+            filmes.add(filme);
+            System.out.println("Filme cadastrado com sucesso!");
+            return true;
+        }
 
     public void buscarFilme(){}
-
 
     /*
     Sugestão:
