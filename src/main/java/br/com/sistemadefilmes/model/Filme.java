fef04116ac2d7c7a27e9a12b3f4d221d2a1d9930
Bruno Silva <fawnsteak@gmail.com>
2023-08-20 19:57:15 -0300

    Criação dos métodos getters e setters de cada atributo da classe filme

--- src/main/java/br/com/sistemadefilmes/model/Filme.java
+++ src/main/java/br/com/sistemadefilmes/model/Filme.java
@@ -12,4 +12,36 @@ public Filme (String nome, String diretor, LocalDate dataDeLancamento, String de
     this.diretor = diretor;
     this.dataDeLancamento = dataDeLancamento;
     this.descricao = descricao;
+
 }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public LocalDate getDataDeLancamento() {
+        return dataDeLancamento;
+    }
+
+    public void setDataDeLancamento(LocalDate dataDeLancamento) {
+        this.dataDeLancamento = dataDeLancamento;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
