a770972cf158c99a8b7be02893209f52f07d1315
Bruno Silva <fawnsteak@gmail.com>
2023-08-20 19:45:08 -0300

    Criação de atributos do Filme e do construtor que irá receber os valores dos atributos

--- src/main/java/br/com/sistemadefilmes/model/Filme.java
+++ src/main/java/br/com/sistemadefilmes/model/Filme.java
@@ -1,6 +1,15 @@
 package br.com.sistemadefilmes.model;
-
+import java.time.LocalDate;
 // Implementar atributos
 public class Filme {
-
+    private String nome;
+    private String diretor;
+    private LocalDate dataDeLancamento;
+    private String descricao;
+}
+public Filme (String nome, String diretor, LocalDate dataDeLancamento, String descricao){
+    this.nome = nome;
+    this.diretor = diretor;
+    this.dataDeLancamento = dataDeLancamento;
+    this.descricao = descricao;
 }
