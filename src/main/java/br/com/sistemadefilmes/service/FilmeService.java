1779a596acce5cfe4473645a6803bf6dd1dccb02
Bruno Silva <fawnsteak@gmail.com>
2023-08-20 18:40:43 -0300

    Criação do método Buscar Filme

--- src/main/java/br/com/sistemadefilmes/service/FilmeService.java
+++ src/main/java/br/com/sistemadefilmes/service/FilmeService.java
@@ -1,12 +1,15 @@
 package br.com.sistemadefilmes.service;
+
 import java.util.ArrayList;
+
 // Implementar métodos e mudar se necessário o tipo de retorno.
 public class FilmeService {
     private ArrayList<Filme> filmes;
+
     public FilmeService() {
         filmes = new ArrayList<Filme>();
 
-    public void cadastrarFilme(Filme filme){
+        public void cadastrarFilme (Filme filme){
             if (filmes.contains(filme)) {
                 System.out.println("O filme já está cadastrado.");
                 return false;
@@ -16,10 +19,28 @@ public class FilmeService {
             return true;
         }
 
-    public void buscarFilme(){}
+        public void buscarFilme () {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Digite o nome do filme que deseja buscar:");
+            String nome = scanner.nextLine();
+            for (Filme filme : filmes) {
+                if (filme.getNome().equals(nome)) {
+                    System.out.println("Filme encontrado:");
+                    System.out.println("Nome: " + filme.getNome());
+                    System.out.println("Diretor: " + filme.getDiretor());
+                    System.out.println("Data de Lançamento: " + filme.getDataDeLancamento());
+                    scanner.close();
+                    return;
+                }
+            }
+            System.out.println("Filme não encontrado.");
+            scanner.close();
+        }
+    }
 
     /*
-    Sugestão:
+    Implementações a serem feitas posteriormente:
+
     public void adicionarDiretor(){}
 
     public void adicionarAtor(){}
