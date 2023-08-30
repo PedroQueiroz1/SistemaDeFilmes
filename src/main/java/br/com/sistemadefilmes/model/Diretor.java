9673b288bad3243c9241251114d99626c6652ad4
Bruno Silva <fawnsteak@gmail.com>
2023-08-30 19:11:45 -0300

    Implementação de Interface Diretor no projeto - *Houveram duvidas*

--- src/main/java/br/com/sistemadefilmes/model/Diretor.java
+++ src/main/java/br/com/sistemadefilmes/model/Diretor.java
@@ -3,9 +3,10 @@ package br.com.sistemadefilmes.model;
 import java.time.LocalDate;
 import java.util.List;
 
-public class Diretor extends Pessoa {
+public interface Diretor extends Pessoa {
     private List<Filme> filmesDirigidos;
 
+    void buscarDiretorPorNome(getPrimeiroNome(), getSegundoNome());
     @Override
     public void apresentar() {
         System.out.println("Sou o diretor " + getPrimeiroNome() + " " + getSegundoNome());
